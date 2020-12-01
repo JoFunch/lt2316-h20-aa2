77b5285ab5614bf6864d760c4dbba46a4edc8bcc
Jonas Funch <floefunch@live.dk>
2020-12-01 18:33:05 +0100

    change of model

--- aa2/model.py
+++ aa2/model.py
@@ -4,32 +4,47 @@ import torch
 import torch.nn.functional as f
 
 
-#Create fully connected network / model
+#C  
+import torch
+import torch.nn as nn
+import torchvision
+import torchvision.transforms as transforms
+
+
+
 
 class RNN(nn.Module):
-	def __init__(self, input_size, hidden_size, num_layers, num_classes, dropout):
-		super(RNN, self).__init__()
-		self.hidden_size = hidden_size
-		self.num_layers = num_layers
-		self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
-		self.fc = nn.Linear(hidden_size * sequence_length, num_classes)
-
-
-
-
-
-	def forward(self, x):
-		h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
-
-
-		#forward
-
-		out, _ = self.rnn(x, h0)
-		out = out.reshape(out.shape[0], -1)
-
-		out = self.fc(out)
-
-		return out
-
-
-
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(RNN, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        # -> x needs to be: (batch_size, seq, input_size)
+        
+        # or:
+        #self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        #self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+        
+    def forward(self, x):
+        # Set initial hidden states (and cell states for LSTM)
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
+        #c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
+        
+        # x: (n, 28, 28), h0: (2, n, 128)
+        
+        # Forward propagate RNN
+        out, _ = self.rnn(x, h0)  
+        # or:
+        #out, _ = self.lstm(x, (h0,c0))  
+        
+        # out: tensor of shape (batch_size, seq_length, hidden_size)
+        # out: (n, 28, 128)
+        
+        # Decode the hidden state of the last time step
+        out = out[:, -1, :]
+        # out: (n, 128)
+         
+        out = self.fc(out)
+        # out: (n, 10)
+        return out
