e80f75d44a50d5a820b34bbad2f4cb874f832338
Jonas Funch <floefunch@live.dk>
2020-12-02 16:40:47 +0100

    model and trainer

--- aa2/model.py
+++ aa2/model.py
@@ -1,5 +1,4 @@
 import torch.nn as nn
-import torch.optim as optim
 import torch
 import torch.nn.functional as f
 
@@ -18,32 +17,24 @@ class RNN(nn.Module):
         super(RNN, self).__init__()
         self.num_layers = num_layers
         self.hidden_size = hidden_size
-        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
-        # -> x needs to be: (batch_size, seq, input_size)
-        
-        # or:
-        #self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
-        #self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        # self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        # self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size, num_classes)
         
     def forward(self, x):
         # Set initial hidden states (and cell states for LSTM)
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
-        #c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
-        
-        # x: (n, 28, 28), h0: (2, n, 128)
-        
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device) 
+                
         # Forward propagate RNN
-        out, _ = self.rnn(x, h0)  
-        # or:
-        #out, _ = self.lstm(x, (h0,c0))  
+    
+        out, _ = self.lstm(x, (h0,c0))  # or x, h
         
         # out: tensor of shape (batch_size, seq_length, hidden_size)
-        # out: (n, 28, 128)
-        
+     
         # Decode the hidden state of the last time step
-        out = out[:, -1, :]
-        # out: (n, 128)
+        out = out[:, -1, :]   
          
         out = self.fc(out)
         # out: (n, 10)
--- aa2/trainer.py
+++ aa2/trainer.py
@@ -1,6 +1,10 @@
 
 import os
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from sklearn.metrics import accuracy_score, recall_score, f1_score, precision_score
+
 
 
 class Trainer:
@@ -11,13 +15,13 @@ class Trainer:
         os.makedirs(dump_folder, exist_ok=True)
 
 
-    def save_model(self, epoch, model, optimizer, loss, scores, hyperparamaters, model_name):
+    def save_model(self, epoch, model, optimizer, loss, scores, hyperparameters, model_name):
         # epoch = epoch
         # model =  a train pytroch model
         # optimizer = a pytorch Optimizer
         # loss = loss (detach it from GPU)
         # scores = dict where keys are names of metrics and values the value for the metric
-        # hyperparamaters = dict of hyperparamaters
+        # hyperparameters = dict of hyperparameters
         # model_name = name of the model you have trained, make this name unique for each hyperparamater.  I suggest you name them:
         # model_1, model_2 etc 
         #  
@@ -29,7 +33,7 @@ class Trainer:
                         'epoch': epoch,
                         'model_state_dict': model.state_dict(),
                         'optimizer_state_dict': optimizer.state_dict(),
-                        'hyperparamaters': hyperparamaters,
+                        'hyperparameters': hyperparameters,
                         'loss': loss,
                         'scores': scores,
                         'model_name': model_name
@@ -38,16 +42,82 @@ class Trainer:
         torch.save(save_dict, os.path.join(self.dump_folder, model_name + ".pt"))
 
 
-    def load_model(self):
+    def load_model(self, path):
         # Finish this function so that it loads a model and return the appropriate variables
-        pass
+
+        checkpoint = torch.load(path)
+        return checkpoint
 
 
-    def train(self, train_X, train_y, val_X, val_y, model_class, hyperparamaters):
+    def train(self, train_X, train_y, val_X, val_y, model_class, hyperparameters):
         # Finish this function so that it set up model then trains and saves it.
-        pass
+        self.train_X = train_X
+        self.train_y = train_y
+        self.val_X = val_X
+        self.val_y = val_y
+        self.model_class = model_class
+
+
+        epochs = hyperparameters['epoch']
+        batch_size = hyperparameters['batch_size']
+        num_classes = hyperparameters['num_classes']
+        learning_rate = hyperparameters['learning_rate']
+        sequence_length = hyperparameters['sequence_length']
+        hidden_size = hyperparameters['hidden_size']
+        num_layers = hyperparameters['num_layers']
+        input_size = hyperparameters['input_size']
+
+        output_size = 4
+
+        self.device = torch.device("cuda:2")
+
+        train_batches = Batcher(train_X, train_y, device, batch_size=batch_size, iteration=epochs)
+
+        model = RNN(input_size, hidden_size, num_layers, num_classes).to(device)
+
+        criterion = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)  
+
+
 
 
     def test(self, test_X, test_y, model_class, best_model_path):
         # Finish this function so that it loads a model, test is and print results.
         pass
+
+
+
+
+
+class Batcher:
+    def __innit__(self, X, y, device, batch_size=50, iteration=None):
+        self.X = X
+        self.Y = Y
+        self.device = device
+        self.batch_size = batch_size
+        self.max_iter=max_iter
+        self.curr_iter = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.curr_iter == self.max_iter:
+            raise StopIteration
+        permutation = torch.randperm(self.X.size()[0], device=self.device)
+        permX = self.X[permunation]
+        permy = self.y[permunation]
+        splitX = torch.split(permX, self.batch_size)
+        splity = torch.split(permy, self.batch_size)
+
+        self.curr_iter =+ 1
+
+        return zip(splitX, splity)
+
+
+
+
+
+
+
+
